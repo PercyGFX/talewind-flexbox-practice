ea17b409b2f04490ad47049f01bd2fe0bc0b9542
PercyGFX <isurangabtk@gmail.com>
2023-06-09 21:22:22 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Header from "./components/header"
 
 function App() {
   return (
-    <div className="container mx-auto p-4 bg-red-500 sm:bg-blue-500 md:bg-green-500 lg:bg-yellow-500 xl:bg-purple-500 ">
-     <h1>Hellohgghfhnp</h1>
+    <div className="container mx-auto shadow-sm">
+     <Header />
     </div>
   );
 }
