da2c92def799f48248eabe062bcaafdc74821524
PercyGFX <isurangabtk@gmail.com>
2023-06-09 15:59:01 +0530

    container breakpoints

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 
 function App() {
   return (
-    <div className="shadow-md">
-     <h1>Hellohgghfh</h1>
+    <div className="container mx-auto p-4 bg-red-500 sm:bg-blue-500 md:bg-green-500 lg:bg-yellow-500 xl:bg-purple-500 ">
+     <h1>Hellohgghfhnp</h1>
     </div>
   );
 }
