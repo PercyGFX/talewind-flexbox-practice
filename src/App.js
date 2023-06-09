e52a7608481f87f62edf9e8c8609e4d96372f805
PercyGFX <isurangabtk@gmail.com>
2023-06-10 02:01:47 +0530

    google fonts installed
    hero & header sections are currently working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header"
+import Hero from "./components/Hero"
 
 function App() {
   return (
-    <div className="container mx-auto shadow-sm">
+    <div className="container mx-auto shadow-sm font-medium bg-gray-50">
      <Header />
+        <Hero />
     </div>
   );
 }
